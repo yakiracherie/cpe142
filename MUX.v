8da54a15fbf4938cfccf8cf528b013de2fd495ad
yakiracherie <yakiracherie@gmail.com>
2016-05-02 13:03:55 -0700

    Update and rename RegDstMUX.v to MUX.v

---
+++ MUX.v
@@ -0,0 +1,15 @@
+module MUX(in0, in1, select, out);
+
+input [15:0] in0, in1;
+input select;
+
+output reg [15:0] out;
+
+always @(*) 
+begin
+	case (select)
+		1'b0: out = in0; 
+		1'b1: out = in1;
+	endcase
+end
+endmodule
--- RegDstMUX.v
+++
@@ -1,3 +0,0 @@
-module RegDstMUX.v();
-
-endmodule

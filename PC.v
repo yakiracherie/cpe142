7e8ef3d4dae2b2745f6b9857349263f765cb2828
yakiracherie <yakiracherie@gmail.com>
2016-05-02 15:42:58 -0700

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,3 +1,19 @@
-module PC();
+`timescale 1ns / 1ns
 
+module PC(clk, rst, PCin, PCout);
+
+input clk, rst;
+input [15:0] PCin;
+
+output reg [15:0] PCout;
+
+always @(posedge clk or negedge reset) 
+begin
+	if (!reset) begin
+		PCout <= PCout;
+	end
+	else begin
+		PCout <= PCin;
+	end
+end
 endmodule

1e77ddc1584bb719ea0a900e37878b223df62e84
JenOng07 <jennifer.ong07@hotmail.com>
2016-05-02 23:40:44 -0700

    Update HazardDetection.v

--- HazardDetection.v
+++ HazardDetection.v
@@ -1,3 +1,22 @@
-module HazardDetection();
-
-endmodule
+module HazardDetectionUnit(IFIDRegRs, IFIDRegRt, IDEXRegRt, IDEXMemRead, PCWrite, IFIDWrite, mux); 
+  input [3:0] IFIDRegRs, IFIDRegRt, IDEXRegRt; 
+  input IDEXMemRead; 
+  output PCWrite, IFIDWrite, mux; 
+  
+  reg PCWrite, IFIDWrite, mux; 
+  
+  always@(IFIDRegRs, IFIDRegRt, IDEXRegRt, IDEXMemRead) 
+    if(IDEXMemRead & ((IDEXRegRt == IFIDRegRs) | (IDEXRegRt == IFIDRegRt))) 
+    begin              // stalling 
+      PCWrite = 0; 
+      IFIDWrite = 0;
+      mux = 1; 
+    end 
+    
+    else begin          //no stalling 
+      PCWrite = 1; 
+      IFIDWrite = 1; 
+      mux = 1; 
+    end 
+ 
+endmodule 
